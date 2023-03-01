6efa78a1f019f11a59041e0610c9b7717413d31a
Jayce Balestrieri <126515139+JayceBal@users.noreply.github.com>
2023-02-28 20:59:27 -0500

    init

---
+++ app.js
@@ -0,0 +1,25 @@
+// Set age var
+let myAge = 18
+
+// Set EarlyYears
+let earlyYears = 2 
+
+earlyYears *= 10.5
+
+// Since we already accounted for the first two years, take the myAge variable, and subtract 2 from it. 
+
+laterYears = myAge - 2
+
+// Calculate Dog Years
+
+laterYears *= 4
+
+// Set my age in dog years
+myAgeInDogYears = earlyYears + laterYears
+
+
+// Set my Name and make it lowecase
+myName = 'Jayce'.toLowerCase()
+
+// Yup
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`)
